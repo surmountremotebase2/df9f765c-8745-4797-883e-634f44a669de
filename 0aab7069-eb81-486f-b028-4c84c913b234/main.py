605026ea30440abd688bcee17ea03801063bbbe4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-05-12 17:30:57 +0500

    User Finee Hh07 saved changes to main.py at 05/12/2026, 12:30:54 UTC time.

--- 0aab7069-eb81-486f-b028-4c84c913b234/main.py
+++ 0aab7069-eb81-486f-b028-4c84c913b234/main.py
@@ -10,7 +10,7 @@ class TradingStrategy(Strategy):
 
     @property
     def interval(self):
-        return "1day"
+        return "5day"
 
     def run(self, data):
         holdings = data["holdings"]
